eae7ab24a59bcc02cbbec8930ef450ddc7339343
Atilla BAYRAM <atillabayramn@gmail.com>
2023-05-31 17:06:22 +0300

    git commit -m 'Fifth Commit'

--- src/Game.java
+++ src/Game.java
@@ -20,10 +20,14 @@ public class Game {
             System.out.println("#####-BÖLGELER-#####");
             System.out.println("1- Güvenli Ev --> Burası sizin için güvenli bir alan, düşman yoktur!");
             System.out.println("2- Mağaza --> Silah ve Zırh satın alabilirsiniz!");
+            System.out.println("0- Çıkış Yap --> Oyunu Sonlandır!");
             System.out.println("Lütfen gitmek istediğiniz bölgeyi seçiniz: ");
             int selectLoc = input.nextInt();
             switch (selectLoc) {
 
+                case 0:
+                    location = null;
+                    break;
                 case 1:
                     location = new SafeHouse(player);
                     break;
@@ -32,6 +36,10 @@ public class Game {
                     break;
                 default:
                     location = new SafeHouse(player);
+            }
+            if(location == null){
+                System.out.println("Bu karanlık ve sisli adadan çabuk vazgeçtin!!");
+                break;
             }
             if (!location.onLocation()) {
                 System.out.println("GAME OVER !!!");
